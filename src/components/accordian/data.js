357c7a92471eae59763c5c2a523e93cc56db8f8a
Himanshu Mehra <himanshumehra@Himanshus-MacBook-Air.local>
2024-11-20 23:44:58 +0530

    Initial commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,25 @@
+const data = [
+    {
+      "id": 1,
+      "question": "What is React?",
+      "answer": "React is a JavaScript library for building user interfaces."
+    },
+    {
+      "id": 2,
+      "question": "What is Vite?",
+      "answer": "Vite is a fast build tool and development server for modern web projects."
+    },
+    {
+      "id": 3,
+      "question": "What are React hooks?",
+      "answer": "React hooks are functions that let you use state and lifecycle features in functional components."
+    },
+    {
+      "id": 4,
+      "question": "What is JSX?",
+      "answer": "JSX stands for JavaScript XML and allows you to write HTML in React."
+    }
+  ];
+
+  export default data;
+  
---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import data from "./data";
+import "./styles.css";
+
+export default function Accordion() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setSnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  function handleMultiSelection(getCurrentId) {
+    let cpyMultiple = [...multiple];
+    const index = cpyMultiple.indexOf(getCurrentId);
+
+    if (index === -1) {
+      cpyMultiple.push(getCurrentId); // Add item if not already present
+    } else {
+      cpyMultiple.splice(index, 1); // Remove item if already present
+    }
+
+    setMultiple(cpyMultiple);
+  }
+
+  return (
+    <div className="wrapper">
+      <button onClick={() => setSnableMultiSelection(!enableMultiSelection)}>
+        Enable multiple
+      </button>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item" key={dataItem.id}>
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title" >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multiple.includes(dataItem.id) && (
+                    <div className="content">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>
+                  )}
+            </div>
+          ))
+        ) : (
+          <div>No data present</div>
+        )}
+      </div>
+    </div>
+  );
+}
